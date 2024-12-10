3b51a76c7ee89f7a5a74c5ee5f49a230b6398094
jakeogh <github.com@v6y.net>
2024-12-09 22:39:57 -0700

    auto-commit

--- compile_kernel/compile_kernel.py
+++ compile_kernel/compile_kernel.py
@@ -1897,6 +1897,15 @@ def check_kernel_config(
         fix=fix,
         url="",
     )
+    verify_kernel_config_setting(
+        path=path,
+        content=content,
+        define="CONFIG_NET_DROP_MONITOR",
+        required_state=True,  # m
+        warn=warn_only,
+        fix=fix,
+        url="",
+    )
     ## genkernel
     # verify_kernel_config_setting(
     #    path=path,
